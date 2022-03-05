ea5873dab34200bd3a619d14f2c2c4f1933e30a0
Michael Davis <jrmdd1496@gmail.com>
2022-03-05 13:34:09 -0600

    commit tree 21e8eb40fc6062c28d06847c82973f4478cc9a8d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+//Saturday fun
+function saturdayFun (activity){
+    if (activity === undefined){
+        return "This Saturday, I want to roller-skate!";
+    }
+    else{
+        return `This Saturday, I want to ${activity}!`;
+    }
+}
+
+//Monday work
+let mondayWork = (function (activity) {
+    if (activity === undefined){
+        return "This Monday, I will go to the office.";
+    }
+    else{
+        return `This Monday, I will ${activity}.`;
+    }
+})
+
+
+//wrap adjective
+function wrapAdjective(wrap){
+    return (function (adjective) {
+        return (adjective === undefined) ? `You are ${wrap}special${wrap}!` : `You are ${wrap}${adjective}${wrap}!`;
+    })
+}
